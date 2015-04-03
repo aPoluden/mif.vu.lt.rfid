c74873eb94a12faf2a1d6ec0d2fa8bf50b33132f
Poluden Artiom <artiom.poluden@mif.stud.vu.lt>
2015-04-03 18:59:58 +0300

    Refactor

--- src/main/java/mif/vu/lt/rfid/app/Application.java
+++ src/main/java/mif/vu/lt/rfid/app/Application.java
@@ -1,50 +1,85 @@
 package mif.vu.lt.rfid.app;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.UnknownHostException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
+import lombok.Getter;
+import mif.vu.lt.rfid.app.controller.ElementController;
 import mif.vu.lt.rfid.app.manager.JsonManager;
 import mif.vu.lt.rfid.app.manager.ParseManager;
+import mif.vu.lt.rfid.app.manager.SequenceManager;
+import mif.vu.lt.rfid.app.manager.SocketManager;
 import mif.vu.lt.rfid.app.manager.XmlManager;
 import mif.vu.lt.rfid.app.model.Receiver;
 import mif.vu.lt.rfid.app.model.Root;
+import mif.vu.lt.rfid.app.model.algorithm.MinMax;
 
 public class Application {
 	
 	ParseManager parser;
-	
-	private void init() throws Exception {
-		String file = "src/main/resources/receivers.xml";
+	@Getter	
+	Root root;
+
+	public void init() throws Exception {
+		String file = "src/main/resources/elements.xml";
 		ParseManager parser = new XmlManager();
-		Root root = (Root) parser.<File>parse(Root.class, new File(file));
-//		parser = new JsonManager();
-//		Receiver receiver = (Receiver) parser.parse(Receiver.class);
+		root = (Root) parser.<File>parse(Root.class, new File(file));
 	}
 	
-	private void run() throws Exception {
+	public void run(int port, int buffSize) throws Exception {
 		
-		try (DatagramSocket socket = new DatagramSocket(7777)) {
-		    
-			byte[] buf = new byte[1024];
-		    DatagramPacket packet = new DatagramPacket(buf, buf.length);
-		    while (true) {
-	            socket.receive(packet);
-		        byte[] byt = packet.getData();
-		        String json = new String(byt);
-		        parser = new JsonManager();
-		        Receiver receiver = (Receiver) parser.<String>parse(Receiver.class, json);
-		    }
-		}
+//		TO - DO controller set Tags
+		ElementController controller = new ElementController();
+		controller.setReceivers(root.getReceivers());
+		controller.setTags(root.getTags());
+		controller.addObserverToReceiver();
+		
+		BlockingQueue<Receiver> queue = new LinkedBlockingQueue<>();
+		SocketManager socketManager = new SocketManager(new JsonManager(), port, buffSize, queue);
+		
+		SequenceManager seqManager = new SequenceManager(new MinMax(), root, queue);
+		
+		Thread socketThread = new Thread(socketManager);
+		socketThread.start();
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+		        try {
+					seqManager.resolveSeq();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+		        try {
+					seqManager.resolveCoords(controller);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+				
 	}
 	
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+    	
+    	int bufferSize = 2048;
+    	int port = 7777;
     	Application app = new Application();
-    	try { 
-//    		app.init();
-    	    app.run();
+    	
+    	try {
+    		
+    		app.init();
+    	    app.run(port, bufferSize);
+    	    
     	} catch (Exception e) { 
     		e.printStackTrace();
     	}
